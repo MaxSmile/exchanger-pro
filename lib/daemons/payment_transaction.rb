5f2f3d1036cc875f16a2bf4b6bdfd34dfecf1a8c
Maxim Lavrinenko <nopster2@gmail.com>
2017-05-19 14:27:12 +0300

    fix daemon

--- lib/daemons/payment_transaction.rb
+++ lib/daemons/payment_transaction.rb
@@ -15,27 +15,6 @@ Signal.trap("TERM") do
 end
 
 while($running) do
-
-  puts 'check_payments'
-  Payment.all.each { |payment|
-    address = payment.payment_address.address
-    next if address.blank?
-    payment_transactions = PaymentTransaction.where(address: address, aasm_state: :confirmed)
-    next if payment_transactions.blank?
-    sum = 0
-    payment_transactions.all.each{ |payment_transaction|
-      sum += payment_transaction.amount
-    }
-    b = BigDecimal.new(payment.amount) - sum
-    pp address + ' ' + sum.to_s + ' ' + payment.amount + ' ' + b.to_s
-    if b <= 0
-      payment.status = Payment::PAYMENT_STATUS_COMPLETE
-    else
-      payment.status = Payment::PAYMENT_STATUS_CREATED
-    end
-    payment.save
-  }
-
   PaymentTransaction::Normal.with_aasm_state(:unconfirm, :confirming).each do |tx|
     begin
       tx.with_lock do
